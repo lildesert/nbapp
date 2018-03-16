dee752e50abdb5f5c367a84be876710cfb6d764a
Julien tabuya <julien@tokywoky.com>
2018-03-17 00:47:02 +0100

    Get games data

--- App.js
+++ App.js
@@ -1,13 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+class Nba extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: "Loading...",
+      games: null
+    };
+  }
+
+  getDateFormat () {
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth() < 9 ? '0' + (today.getMonth() + 1) : today.getMonth() + 1;
+    let day = today.getDate() < 9 ? '0' + today.getDate() : today.getDate();
+    //let day = 17;
+    let date = '' + year + month + day;
+
+    return date;
+  }
+
+  componentDidMount () {
+    const currentDate = this.getDateFormat();
+
+    const query = new Request('http://data.nba.net/10s/prod/v1/' + currentDate + '/scoreboard.json');    
+    return fetch(query)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          display: "coucou",
+        }, function(){});
+      })
+      .catch((error) => {
+          console.error("error");
+      });
+  }
+
+  render() {
+    let prop = "some property";
+    return (
+      <View style={styles.container}>
+        <Text>{this.state.display}</Text>
+        <Text>{this.state.games}</Text>
+      </View>
+    );
+  }
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Nbapp</Text>
+        <Nba />
       </View>
     );
   }
@@ -19,5 +65,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
