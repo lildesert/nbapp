671140817225b1f4303ef8986601173fc7206a99
lildesert <julien.bistoquet@gmail.com>
2018-07-04 18:53:51 +0200

    api: rename Games to GamesApi

--- api/GamesApi.js
+++ api/GamesApi.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 // Fetch all games of the day
-function getGames (date) {
-  const query = new Request('http://data.nba.net/10s/prod/v1/' + date + '/scoreboard.json');    
+// http://data.nba.net/10s/prod/v1/20180421/scoreboard.json
+function getGames(date) {
+  const query = new Request('http://data.nba.net/10s/prod/v1/' + date + '/scoreboard.json');
   return fetch(query)
     .then(response => response.json())
     .then(json => {
@@ -14,7 +15,8 @@ function getGames (date) {
 }
 
 // get data of the game matching the date and gameId
-function getGame (date, gameId) {  
+// http://data.nba.net/data/10s/prod/v1/20180421/0041700164_boxscore.json
+function getGame(date, gameId) {
   const query = new Request('http://data.nba.net/data/10s/prod/v1/' + date + '/' + gameId + '_boxscore.json');
   return fetch(query)
     .then(response => response.json())
